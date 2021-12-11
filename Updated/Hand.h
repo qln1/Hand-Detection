aded1b8be376f1b400744c135832bbd3734ea8ff
qln1 <83885260+qln1@users.noreply.github.com>
2021-12-10 18:34:42 -0800

    Add files via upload

---
+++ Updated/Hand.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+#include <iostream>
+#include <cmath>
+#include <opencv2/core/types.hpp>
+#include <vector>
+#include <stdlib.h>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video.hpp>
+using namespace cv;
+using namespace std;
+
+struct Hand {
+	Point location = Point(-1, -1);
+	int type = -1;
+};
